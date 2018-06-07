6ff920be23b14186822d81a0dcd0b74374246bb6
x12986 <1092181938@qq.com>
2018-06-07 19:11:53 +0800

    测试题

--- src/main/Demo1.java
+++ src/main/Demo1.java
@@ -1,3 +1,7 @@
+import com.sun.deploy.util.ArrayUtil;
+
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 /**
  * Demo1
@@ -8,6 +12,12 @@ import java.util.Scanner;
 public class Demo1 {
     public static void main(String[] args){
         Scanner sc=new  Scanner(System.in);
+        new ArrayList<>().sort(new Comparator<Object>() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                return 0;
+            }
+        });
         int n=sc.nextInt();
         String name = sc.nextLine();
         sc.close();
